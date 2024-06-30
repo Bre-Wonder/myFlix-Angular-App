d5688648a16d268c95c14ee776c0e5b117a22e86
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-06-29 18:40:54 -0700

    Passed Angular Material dialog in the constructor as an arguemet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'myFlix-Angular-App';
+
+  constructor(public dialog: MatDialog) { }
+//Function opens up dialog when the signup button is clicked
+openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+  //Assigning the dialog a width
+    width: '280px'
+    });
+  }
 }
