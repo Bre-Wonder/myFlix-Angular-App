469f1cf39fd637805ec52055a04ce449dd88260f
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-06-29 17:48:27 -0700

    added logic for user-registration

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+//Closes dialog on success of user registration
+import { MatDialogRef } from '@angular/material/dialog';
+
+//Brings in API calls
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+//Disiplay notifications back to user
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
-  styleUrl: './user-registration-form.component.scss'
+  styleUrl: ['./user-registration-form.component.scss']
 })
-export class UserRegistrationFormComponent {
+export class UserRegistrationFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+constructor(
+  public fetchApiData: FetchApiDataService,
+  public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+  public snackBar: MatSnackBar) {}
+
+ngOnInit(): void {
+}
+
+//Send the inputs from the form to the back end
+registerUser(): void {
+  this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+    //Come back later - the logic for successful user registration goes here!!
+    this.dialogRef.close();
+    this.snackBar.open(result, 'OK', {
+      duration: 2000
+    });
+  }, (result) ={
+    this.snackBar.open(result, 'OK', { 
+      duration: 2000    
+    });
+  });
+}
 
 }
