0d663e39b592f765c2563224138137be97f3bd87
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-06-29 19:35:03 -0700

    updated typo

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -13,7 +13,7 @@ import { response } from 'express';
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
-  styleUrl: ['./user-registration-form.component.scss']
+  styleUrls: ['./user-registration-form.component.scss']
 })
 
 export class UserRegistrationFormComponent implements OnInit {
