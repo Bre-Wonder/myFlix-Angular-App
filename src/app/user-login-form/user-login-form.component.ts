98afa2228416414669f6ad34b3585b8dacbed4d3
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-07-06 14:27:24 -0700

    created the start of Login Form in Angular

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { MatDialogRef } from '@angular/material/dialog';
+
+//brings in API call from ts file
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrl: './user-login-form.component.scss'
+  styleUrls: ['./user-login-form.component.scss']
 })
-export class UserLoginFormComponent {
+export class UserLoginFormComponent  implements OnInit {
 
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+  //sets up initialization tasks for a user to login
+  ngOnInit(): void {
+  }
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+      //handle successful login here
+      this.dialogRef.close();
+      this.snackBar.open('You Logged in Successfully', 'OK', {
+        duration: 2000
+      });
+    }, (error) => {
+      console.log(error);
+      this.snackBar.open('Login Unsuccessful ' + error.message, 'OK', {
+        duration: 2000
+      });
+    });
+  }
 }
