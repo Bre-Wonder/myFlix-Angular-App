e07fb48090ad3f4311f384e81311112fe37f2694
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-07-06 15:43:38 -0700

    added method for User Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -11,6 +12,7 @@ export class AppComponent {
   title = 'myFlix-Angular-App';
 
   constructor(public dialog: MatDialog) { }
+
 //Function opens up dialog when the signup button is clicked
 openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
@@ -18,4 +20,13 @@ openUserRegistrationDialog(): void {
     width: '280px'
     });
   }
+
+//Function opens up dialog when the login button is clicked
+openUserLoginDialog(): void {
+  this.dialog.open(UserLoginFormComponent, {
+    width: '280px'
+  });
+}
+
+
 }
