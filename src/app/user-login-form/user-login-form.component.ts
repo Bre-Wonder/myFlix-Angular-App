697cbd4f90a70e33bd55f611362bbcb4feb36747
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-07-15 16:35:31 -0700

    added more routes

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -6,6 +6,8 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
@@ -18,7 +20,8 @@ export class UserLoginFormComponent  implements OnInit {
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private router: Router
   ) { }
 
   //sets up initialization tasks for a user to login
@@ -34,6 +37,7 @@ export class UserLoginFormComponent  implements OnInit {
       this.snackBar.open('You Logged in Successfully', 'OK', {
         duration: 2000
       });
+      this.router.navigate(['movies']);
     }, (error) => {
       console.log(error);
       this.snackBar.open('Login Unsuccessful ' + error.message, 'OK', {
