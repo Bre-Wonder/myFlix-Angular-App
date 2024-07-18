f660a5a7bfc9015b8aa2f132e2b94c1ef9dc0869
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-07-18 16:39:23 -0700

    created dialog box in angular comoponent

--- src/app/movie-snapshot/movie-snapshot.component.ts
+++ src/app/movie-snapshot/movie-snapshot.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-snapshot',
   templateUrl: './movie-snapshot.component.html',
-  styleUrl: './movie-snapshot.component.scss'
+  styleUrls: ['./movie-snapshot.component.scss']
 })
-export class MovieSnapshotComponent {
+export class MovieSnapshotComponent implements OnInit {
 
-}
+  public movieData: {
+    movieTitle: string;
+    description: string;
+  }
+
+  constructor(
+    public dialogRef: MatDialogRef<MovieSnapshotComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.movieData = {
+      movieTitle: data.movieTitle,
+      description: data.description
+    };
+  }
+
+  ngOnInit() : void {  
+  }
+
+
+  }
