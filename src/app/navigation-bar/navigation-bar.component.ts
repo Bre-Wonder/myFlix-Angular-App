3885560f56abaf48092130f98472e85d20391222
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-07-20 15:42:35 -0700

    changed styles to an array

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.scss'
+  styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent {
 
