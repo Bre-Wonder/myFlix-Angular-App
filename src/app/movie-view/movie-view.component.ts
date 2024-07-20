5095646318603de9e4d905a927642831875beab3
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-07-20 12:23:38 -0700

    updated style array and imported components

--- src/app/movie-view/movie-view.component.ts
+++ src/app/movie-view/movie-view.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 
 @Component({
   selector: 'app-movie-view',
   templateUrl: './movie-view.component.html',
-  styleUrl: './movie-view.component.scss'
+  styleUrls: ['./movie-view.component.scss']
 })
 export class MovieViewComponent {
 
