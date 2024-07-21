dc20021a148aab5e971d5fefb111529e62f6b33a
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-07-20 17:33:09 -0700

    added function to allow user to update info

--- src/app/update-user/update-user.component.ts
+++ src/app/update-user/update-user.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-update-user',
   templateUrl: './update-user.component.html',
-  styleUrl: './update-user.component.scss'
+  styleUrls: ['./update-user.component.scss']
 })
-export class UpdateUserComponent {
+export class UpdateUserComponent implements OnInit {
 
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UpdateUserComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+
+  ngOnInit(): void {
+
+  }
+
+  updateUser(): void {
+    this.fetchApiData.updateUser(this.userData.Username, this.userData).subscribe((result) => {
+
+      this.dialogRef.close();
+      this.snackBar.open(result, 'User Info Updated Successfully', {
+        duration: 200
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', { 
+        duration: 2000
+      });
+    });
+  }
 }
