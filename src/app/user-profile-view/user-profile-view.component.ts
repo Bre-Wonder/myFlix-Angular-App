b7e786135a7037c22da5bd9edd7fe89267c9d037
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-07-20 17:34:27 -0700

    allow user to open dialog from profile view

--- src/app/user-profile-view/user-profile-view.component.ts
+++ src/app/user-profile-view/user-profile-view.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
+import { Component, OnInit, Input } from '@angular/core';
+import { UpdateUserComponent } from '../update-user/update-user.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-profile-view',
   templateUrl: './user-profile-view.component.html',
-  styleUrl: './user-profile-view.component.scss'
+  styleUrls: ['./user-profile-view.component.scss']
 })
-export class UserProfileViewComponent {
+export class UserProfileViewComponent implements OnInit{
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+
+   //Function opens up dialog when the update button is pressed in the user profile view
+   openUpdateUserDialog(): void {
+    this.dialog.open(UpdateUserComponent, {
+    width: '280px'
+    });
+  }
 
 }
