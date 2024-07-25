659a452acfcc974acf34c1d78632de827ae71c65
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-07-24 20:48:43 -0700

    tried to pass data into dialog

--- src/app/director-info/director-info.component.ts
+++ src/app/director-info/director-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-director-info',
@@ -10,6 +12,8 @@ export class DirectorInfoComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<DirectorInfoComponent>,
+    public snackBar: MatSnackBar,
+    public fetchApiData: FetchApiDataService,
     @Inject(MAT_DIALOG_DATA) 
     public data: {
       director: string;
@@ -20,6 +24,20 @@ export class DirectorInfoComponent implements OnInit {
   ) { }
 
   ngOnInit() : void {  
+  }
+
+  //come back to this funciton - I need to understand from fetchAPI until the end how the data is passed. 
+  getDirector() : void {
+    this.fetchApiData.getDirector(this.data.director).subscribe((response) => {
+      this.dialogRef.close();
+      console.log(response);
+      
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
   }
 
 }
