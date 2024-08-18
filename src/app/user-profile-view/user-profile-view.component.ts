7da312900565039483cc46750c314af69aeb0a04
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-08-18 11:35:07 -0700

    added console.log on line 69

--- src/app/user-profile-view/user-profile-view.component.ts
+++ src/app/user-profile-view/user-profile-view.component.ts
@@ -66,6 +66,7 @@ export class UserProfileViewComponent implements OnInit{
 
   //Delete User from localStorage - NOT WORKING CORRECTLY - needs to come from the fetchApiData file
   deleteUser(): void {
+    console.log('Delete Button was pressed');
     localStorage.removeItem('user');
     localStorage.removeItem('token');
     console.log('User Cleared from localStorage');
