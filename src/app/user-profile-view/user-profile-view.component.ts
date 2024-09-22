a52c34ba089b6425088120275104b73450f55fca
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-09-21 18:58:19 -0700

    took out function not being used

--- src/app/user-profile-view/user-profile-view.component.ts
+++ src/app/user-profile-view/user-profile-view.component.ts
@@ -68,32 +68,6 @@ export class UserProfileViewComponent implements OnInit{
       });
     }
 
-  // getFavoriteMovies(): void {
-  //   this.fetchApiData.getUser(this.userData.Username).subscribe((resp: any) => {
-  //     if (this.movies.length === 0) {
-  //       this.noMovies = 'You have not selected any movies';
-  //     }, 
-  //     if (!userLgoin) {
-  //       this.userMessage = 'Please login to see movie messages';
-  //     }
-
-  //   });
-
-
-  // }
-
-  
-
-  //User Add favrotie movie to their profile
-  
-  
-  //#3 Create a users list of favorite movie
-  //#4 Display Movie Cards in UI if user has selected a movie
-  //#5 Put a message in if the user has not selected any movies.. aka empty array
-  
-  //Create like button to toggle
-
-
     
    //Function opens up dialog when the update button is pressed in the user profile view
    openUpdateUserDialog(): void {
