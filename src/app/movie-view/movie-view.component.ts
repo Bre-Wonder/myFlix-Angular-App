bf9b2e2c8302d4bd97c07b8c70e3724339e30984
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-01-19 08:41:23 -0800

    moved function ove to get all movies

--- src/app/movie-view/movie-view.component.ts
+++ src/app/movie-view/movie-view.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-movie-view',
   templateUrl: './movie-view.component.html',
   styleUrls: ['./movie-view.component.scss']
 })
+
+
 export class MovieViewComponent {
+  movies: any[] = [];
+
+  constructor(
+    public fetchApiData: FetchApiDataService
+  ){}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+        this.movies = resp;
+        console.log(this.movies);
+        return this.movies;
+      });
+    }
 
 }
