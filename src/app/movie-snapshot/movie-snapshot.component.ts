b6859f2882816c6207f7e9398ef201a8e3de6872
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-06-17 11:11:30 -0700

    added documentation generated through Copilot

--- src/app/movie-snapshot/movie-snapshot.component.ts
+++ src/app/movie-snapshot/movie-snapshot.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+/**
+ * Component responsible for displaying a snapshot of movie details in a dialog.
+ */
 @Component({
   selector: 'app-movie-snapshot',
   templateUrl: './movie-snapshot.component.html',
@@ -8,11 +11,22 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class MovieSnapshotComponent implements OnInit {
 
+  /**
+   * Object containing movie details to be displayed.
+   * @property {string} movieTitle - The title of the movie.
+   * @property {string} description - A brief description of the movie.
+   */
   public movieData: {
     movieTitle: string;
     description: string;
   }
 
+  /**
+   * Creates an instance of MovieSnapshotComponent.
+   *
+   * @param dialogRef - Reference to the dialog containing the component.
+   * @param data - Injected data object containing the movie information.
+   */
   constructor(
     public dialogRef: MatDialogRef<MovieSnapshotComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
@@ -23,6 +37,9 @@ export class MovieSnapshotComponent implements OnInit {
     };
   }
 
+  /**
+   * Angular lifecycle hook that is called after component initialization.
+   */
   ngOnInit() : void {  
   }
 
