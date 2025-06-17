91f7970af3264b7767979eb663e66a5ab8836b6f
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-06-17 10:31:12 -0700

    added documentation

--- src/app/genre-info/genre-info.component.ts
+++ src/app/genre-info/genre-info.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+/**
+ * GenreInfoComponent displays detailed information about a specific genre
+ * in a modal dialog.
+ *
+ * It receives genre data through Angular Material's dialog data injection.
+ */
+
 @Component({
   selector: 'app-genre-info',
   templateUrl: './genre-info.component.html',
   styleUrls: ['./genre-info.component.scss']
 })
+
+/**
+ * Creates an instance of GenreInfoComponent.
+ *
+ * @param dialogRef Reference to the dialog opened for this component.
+ * @param data The injected data containing genre information.
+ * @param data.genre The name of the genre.
+ * @param data.description A brief description of the genre.
+ */
+
 export class GenreInfoComponent implements OnInit {
 
   constructor(
@@ -16,6 +33,10 @@ export class GenreInfoComponent implements OnInit {
       description: string;
     }
   ) { }
+
+/**
+ * Lifecycle hook that is called after data-bound properties are initialized.
+ */
 
   ngOnInit() : void {  
   }
